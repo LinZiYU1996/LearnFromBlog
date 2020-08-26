a30ab981cbd744e564ae68f6726dfb8cdee59720
LinZiYu1996 <2669093302@qq.com>
2020-08-26 15:54:01 +0800

    8-26

---
+++ my-annotation-demo/src/test/java/com/lin/myannotationdemo/MyAnnotationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.myannotationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnnotationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
